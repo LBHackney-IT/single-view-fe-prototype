67e9758e2393fd87a13a15cd0b279e4b07e6ab46
Charli Wild <charli.wild@charlis-mbp.lan>
2022-02-18 11:48:04 +0000

    added functionality to scratchpad

--- src/components/ScratchPad.tsx
+++ src/components/ScratchPad.tsx
@@ -14,14 +14,24 @@ export const ScratchPad = (): JSX.Element => {
           value={content}
           aria-describedby="more-detail-hint"
           onChange={(e) => setContent(e.target.value)}
+          placeholder="This is a place to jot down any notes during the call."
         ></textarea>
+        <button
+          className="govuk-button govuk-secondary"
+          style={{ marginTop: "1rem" }}
+          hidden={!open}
+          onClick={() => setContent("")}
+        >
+          Clear
+        </button>
       </div>
+
       <button
         aria-expanded={open}
         className="lbh-collapsible__button"
         onClick={() => setOpen(!open)}
       >
-        <h2 className="lbh-heading-h2 lbh-collapsible__heading">Scratchpad</h2>
+        <h2 className="lbh-heading-h3 lbh-collapsible__heading">Scratchpad</h2>
         <svg width="17" height="10" viewBox="0 0 17 10">
           <path d="M2 1.5L8.5 7.5L15 1.5" strokeWidth="3" />
         </svg>
