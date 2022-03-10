e0f537f723b6986f6c26bc144d696ae56a013b91
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-03-10 16:20:47 +0000

    Configure s3 with error document; remove cloudfront custom error responses

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -11,18 +11,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   enabled             = true
   default_root_object = "index.html"
 
-  custom_error_response {
-    error_code         = "404"
-    response_code      = "200"
-    response_page_path = "/index.html"
-  }
-
-  custom_error_response {
-    error_code         = "403"
-    response_code      = "200"
-    response_page_path = "/index.html"
-  }
-
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
