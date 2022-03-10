7ea8208b4924ccabf401257f6088ae712866c11a
Charli Wild <charli.wild@charlis-mbp.lan>
2022-03-10 10:01:43 +0000

    fixed auth import

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
-import { login } from "../auth";
+import { login } from "../Auth";
 
 export const LoginView = (): JSX.Element => {
-    const [fullName, setFullName] = useState("");
+  const [fullName, setFullName] = useState("");
 
-    const handleLogin = (event: React.SyntheticEvent) => {
-        event.preventDefault();
-        login(fullName) && (window.location.href = "/");
-    }
+  const handleLogin = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    login(fullName) && (window.location.href = "/");
+  };
 
-    return (
-        <>
-            <h1 className="lbh-heading-h1">Sign in</h1>
-            <form onSubmit={handleLogin} method="POST">
-                <div className="govuk-form-group lbh-form-group">
-                <label className="govuk-label lbh-label">Full Name</label>
-                    <input
-                    className="govuk-input lbh-input"
-                    id="fullName"
-                    name="fullName"
-                    type="text"
-                    value={fullName}
-                    onChange={(e) => setFullName(e.target.value)}
-                    />
-                </div>
-                <button
-                    className="govuk-button lbh-button"
-                    type="submit"
-                >
-                    Sign in
-                </button>
-            </form>
-        </>
-    );
-}
+  return (
+    <>
+      <h1 className="lbh-heading-h1">Sign in</h1>
+      <form onSubmit={handleLogin} method="POST">
+        <div className="govuk-form-group lbh-form-group">
+          <label className="govuk-label lbh-label">Full Name</label>
+          <input
+            className="govuk-input lbh-input"
+            id="fullName"
+            name="fullName"
+            type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+        </div>
+        <button className="govuk-button lbh-button" type="submit">
+          Sign in
+        </button>
+      </form>
+    </>
+  );
+};
