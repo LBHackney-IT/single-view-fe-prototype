614ff02c889cf1944286ab891aa829ca6e83708c
Charli Wild <charli.wild@charlis-mbp.lan>
2022-03-16 14:19:45 +0000

    implemented bell, need better quality svg

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import bell from "../assets/images/whitebellv2.png";
 import Notifications from "react-notifications-menu";
 
 export const NotificationsComponent = (): JSX.Element => {
@@ -16,7 +17,8 @@ export const NotificationsComponent = (): JSX.Element => {
 
   const mockNotificationData: NotificationData[] = [
     {
-      image: "",
+      image:
+        "https://cdn.iconscout.com/icon/free/png-256/list-message-2367725-1976875.png",
       message: "A new note requires your attention.",
       detailPage: "/records/afa799a2-1c7e-48d6-bc8e-9fe652e56c16#notes",
       receivedTime: "1h ago",
@@ -24,12 +26,14 @@ export const NotificationsComponent = (): JSX.Element => {
   ];
 
   return (
-    <p onClick={onNotificationClick}>
-      Notifications
-      <Notifications
-        data={mockNotificationData}
-        markAsRead={onNotificationClick}
-      />
-    </p>
+    <div className="notificationsDiv">
+      <p onClick={onNotificationClick}>
+        <Notifications
+          data={mockNotificationData}
+          markAsRead={onNotificationClick}
+          icon={bell}
+        />
+      </p>
+    </div>
   );
 };
