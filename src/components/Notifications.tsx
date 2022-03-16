d0d20ce362f72a47361c30b7f428a54d721fa312
Charli Wild <charli.wild@charlis-mbp.lan>
2022-03-16 12:14:15 +0000

    added notification badge

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,3 +1,17 @@
-export const Notifications = () => {
-  return <p>Notifications</p>;
+import { useState } from "react";
+import NotificationBadge from "react-notification-badge";
+import { Effect } from "react-notification-badge";
+
+export const Notifications = (): JSX.Element => {
+  const [notificationCount, setnotificationCount] = useState(1);
+  const onNotificationClick = () => {
+    setnotificationCount(0);
+  };
+
+  return (
+    <p onClick={onNotificationClick}>
+      Notifications{" "}
+      <NotificationBadge count={notificationCount} effect={Effect.SCALE} />
+    </p>
+  );
 };
