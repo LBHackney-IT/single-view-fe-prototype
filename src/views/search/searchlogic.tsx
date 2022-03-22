451b290be862980ff834447019562c0302e04d2c
Charli Wild <charli.wild@charlis-mbp.lan>
2022-03-22 16:49:47 +0000

    search by either name/dob or postcode

--- src/views/search/searchlogic.tsx
+++ src/views/search/searchlogic.tsx
@@ -1,16 +1,37 @@
 import axios from "axios";
 
-export const SearchByResident = (): Promise<any> => {
-  let authToken = getAuthToken();
+type searchParams = {
+  firstName?: string;
+  lastName?: string;
+  dateOfBirth?: string;
+  postCode?: string;
+};
 
-  let body; //add search params here
+export const SearchByResident = (searchParams: searchParams): Promise<any> => {
+  let result;
 
   try {
-    let result = axios.post(process.env.HOUSING_SEARCH_API_STAGING_URL, body, {
-      headers: {
-        Authorization: `${authToken}`,
-      },
-    });
+    if (searchParams.postCode.length === 6) {
+      result = axios.get(
+        `${process.env.SEARCH_API_STAGING_URL}/search/persons?postcode=${searchParams.postCode}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.HOUSING_SEARCH_API_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    } else {
+      result = axios.get(
+        `${process.env.HOUSING_SEARCH_API_STAGING_URL}/search/persons?firstName=${searchParams.firstName}&lastName=${searchParams.lastName}&dateOfBirth=${searchParams.dateOfBirth}`,
+        {
+          headers: {
+            Authorization: `${process.env.HOUSING_SEARCH_API_TOKEN}`,
+          },
+        }
+      );
+    }
+
     return result;
   } catch (err) {
     console.log(err);
@@ -19,26 +40,4 @@ export const SearchByResident = (): Promise<any> => {
   //need to sign api call with hackney token - as we don't have this in the front end yet, will need to spoof UI access. Can this be done through the UI? Or will there be CORS issues
   //https://app.swaggerhub.com/apis/Hackney/searchApi/1.0.0
   //process.ENV.HOUSING_SEARCH_API
-};
-
-//the below call is just to get an auth token, which will be provided by the login screen earlier in the journey
-const getAuthToken = async (): Promise<any> => {
-  let tokenRequestBody = {
-    requestedBy: "charli.wild@hackney.gov.uk",
-    authorizedBy: "charli.wild@hackney.gov.uk,",
-    consumerType: "user",
-    apiName: "housing-search-api",
-    apiEndpoint: "/search/persons",
-    environment: "staging",
-    dateRequested: "2022-03-22",
-  };
-
-  const response: any = axios.post(
-    process.env.AUTH_TOKEN_GENERATOR_STAGING_URL,
-    tokenRequestBody
-  );
-
-  return response.body.token;
-
-  //https://app.swaggerhub.com/apis/Hackney/authorisation-token-generator-api/1.0#/tokenRequest
 };
