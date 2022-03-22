75f327657558b9642e92e339af10b553a26f0fab
Charli Wild <charli.wild@charlis-mbp.lan>
2022-03-22 12:07:16 +0000

    pseduocode for search

---
+++ src/views/search/searchlogic.tsx
@@ -0,0 +1,4 @@
+export const SearchByResident = () => {
+  //need to sign api call with hackney token - as we don't have this in the front end yet, will need to spoof UI access
+  //https://app.swaggerhub.com/apis/Hackney/searchApi/1.0.0
+};
